e5c01dde78a49ba1e306f23dd598cf2152a02d71
ERIC KELLY <eakelly220@gmail.com>
2018-03-25 21:53:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  var price = Math.floor(Math.random(1)*100+1 );
- var getCartart = {itemName:item, itemPrice:price};
+ var getCart = {itemName:item, itemPrice:price};
  return `${item} has been added to your cart.`;
  
 }
